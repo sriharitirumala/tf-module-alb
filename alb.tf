75e11d724aa855e020ffb0adff513f79f6d11d77
sriharitirumala <sritirumala30@gmail.com>
2023-03-30 14:26:56 +0530

    adding vpc subnets, routes, route table associations

--- alb.tf
+++ alb.tf
@@ -1,13 +1,41 @@
-resource "aws_lb" "test" {
+resource "aws_lb" "main" {
   name               = "${var.name}-${var.env}"
   internal           = var.internal
   load_balancer_type = var.load_balancer_type
   subnets            = var.subnets
   enable_deletion_protection = var.enable_deletion_protection
+  security_groups = [aws_security_group.main.id]
 
   tags = merge(
     var.tags,
     { Name = "${var.name}-${var.env}" }
   )
 
+}
+
+resource "aws_security_group" "main" {
+  name        = "${var.name}-${var.env}-lb"
+  description = "${var.name}-${var.env}-lb"
+  vpc_id = var.vpc_id
+
+
+  ingress {
+    description      = "app"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = merge (
+    var.tags,
+    { Name = "${var.name}-${var.env}" }
+  )
 }
